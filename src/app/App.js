f8c95adadc797ed31a71649019a840b82b3f862f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2017-11-25 11:48:48 +0530

    removed starter

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,10 @@
-
-//import from node_modules
 import React from "react";
-
-//default import
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-import Home from "./components/Home";
-import About from "./components/About";
-
+ 
 export class App extends React.Component {
-    //keyword
-    //return a virtual DOM
+    
     render() {
-        //JSX
-        return (
-            <div>
-                <Header  title="React App" />
-                <Home />
-                <About />
-                <Footer year={2017} company="React App" />
-            </div>
-        )
+        return React.createElement('h1', {},
+                        'Welcome to React');
         
     }
 }
